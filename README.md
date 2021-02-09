4ce9cd66209f6d97d2f0102698cd6493c5ee892b
Bethold <mtaycruz@gmail.com>
2021-02-08 19:32:10 -0500

    test to see if table contents work

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Professional README Generator Starter Code
+# professional-README-generator
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Install](#install)
+  - [Usage](#usage)
+  - [Contribute](#contribute)
+  - [Test](#test)
 
-[How to create a Professional README](./readme-guide.md)
+  ## Description
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lectus ante, tempor sed elit et, consectetur euismod nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas ac dapibus sapien, et imperdiet ante. Vestibulum placerat turpis sed lorem elementum faucibus. Mauris ultrices, magna vitae eleifend laoreet, mauris mi consectetur est, eu lacinia velit arcu cursus purus. Mauris eget tortor ut neque tempus maximus. In viverra erat eget est lacinia dapibus. Suspendisse cursus dui sapien, ac facilisis lacus porttitor vitae. Integer tincidunt cursus justo quis tincidunt. Fusce non libero massa. Quisque scelerisque pellentesque ipsum ac bibendum. Nunc vestibulum tellus mi, venenatis laoreet odio consectetur ac. Pellentesque mi diam, porta in tortor ut, tristique fermentum magna. In hac habitasse platea dictumst.
+
+  ## Usage
+  Aenean nec elementum lacus, eu tempor risus. Donec rutrum vehicula purus a molestie. Sed suscipit et sapien id egestas. Phasellus feugiat purus metus, non posuere augue egestas sit amet. Integer nisl lectus, consectetur quis lobortis sit amet, rhoncus a orci. Morbi a rhoncus est, vel porttitor lectus. Suspendisse et porta ante, vel sagittis nisl. Sed id arcu dui. Nullam mattis ipsum pharetra nulla lacinia, vel consectetur purus dapibus. Suspendisse ante ligula, vestibulum quis blandit eu, placerat laoreet ante. Aenean turpis turpis, facilisis non molestie non, vestibulum nec velit. Maecenas sed odio eget nisl venenatis condimentum. Nunc ultrices ante ligula, et fermentum massa porttitor vel.
+
+  ## Contribute
+  Donec porta odio id urna luctus pellentesque. Cras sed quam sed nunc varius cursus. Aliquam laoreet neque ut ornare blandit. Proin venenatis congue tortor et placerat. In ut nisl mi. Praesent non libero id turpis semper blandit non ut mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus iaculis dapibus hendrerit. Donec ut mattis felis. Nunc nec sapien id turpis fringilla placerat nec nec arcu.
+
+  ## Test
+  Donec sit amet iaculis erat, non feugiat nibh. Etiam cursus risus augue, non ullamcorper ipsum egestas sit amet. Cras urna lectus, facilisis et diam et, condimentum sagittis sem. In hac habitasse platea dictumst. Nam at finibus enim. Praesent ultricies dictum malesuada. Etiam velit quam, sagittis sagittis est nec, pharetra lobortis ligula. In tristique est lacus, sed condimentum metus malesuada quis. Curabitur scelerisque est a metus consequat finibus. Donec erat enim, mollis non auctor vitae, varius ac lectus.
--- index.js
+++ index.js
@@ -1,13 +1,108 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application - done
+const inquirer = require('inquirer');
+const { rejects } = require('assert');
+const fs = require('fs');
+const { resolve } = require('path');
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+            if(nameInput){
+                return true;
+            } else {
+                console.log('Please enter your project Name.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+          if(nameInput){
+            return true;
+          } else{
+            console.log('Please enter a description.');
+            return false;
+          }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Please provide the installation instructions. (Required)',
+        validate: nameInput => {
+          if(nameInput){
+            return true;
+          } else{
+            console.log('Please enter installation instructions.');
+            return false;
+          }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide the usage information. (Required)',
+        validate: nameInput => {
+            if(nameInput){
+              return true;
+            } else{
+              console.log('Please enter the usage information');
+              return false;
+            }
+          }
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Please provide the Contribution guidlines.(Required)'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide the test instructions. (Required)'
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+
+            if(err){
+                reject(err)
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File Created!'
+            })
+        });
+    });
+
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    
+    const response = await inquirer.prompt(questions);
+    console.log(response);
+
+    const markdown = generateMarkdown(response);
+    
+
+
+    await writeToFile("./README.md", markdown);
+
+}
 
 // Function call to initialize app
 init();
